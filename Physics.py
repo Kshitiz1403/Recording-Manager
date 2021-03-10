73bf268bfcf514c14bd2c69d497b1dfc8787c5e1
Kshitiz1403 <75060259+Kshitiz1403@users.noreply.github.com>
2021-03-10 17:45:00 +0530

    Add files via upload

---
+++ Physics.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+
+source = "E:\\OneDrive - vitbhopal.ac.in\\OBS Classes"
+destination = "E:\\OneDrive - vitbhopal.ac.in\\Classes\\Physics\\March"
+
+for i in reversed(os.listdir(source)):
+    print("File being moved is: "+i)
+    sourcefile = source+'\\'+i
+    #source to destination
+    dest = shutil.move(sourcefile,destination)
+    print("Successful, file",i,"is moved to",destination, "from", source)
+
+    print("File renaming in process")
+    destinationfile = destination + '\\' + i
+    date = i[8:10]
+    file_name = date + " Mar Physics.mp4"
+    newfile = destination + '\\' + file_name
+    os.rename(destinationfile,newfile)
+    print("File",i,"renamed to",file_name)
+    break
